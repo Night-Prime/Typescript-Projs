f09d79d38a4fa39911d9d5a827a21eec035ef068
Daniel Abati <danielabatibabatunde1@gmail.com>
2022-05-18 09:41:57 +0100

    ts-learn

--- ts/index.ts
+++ ts/index.ts
@@ -11,3 +11,14 @@ const noDefinition:undefined = undefined;
 const joinstring = function(a:string, b:string):string {
   return 'Dhaniel'
 }
+
+
+// Working with Decorators
+
+const Component = ( target: any) => { // <--- this is a plain decorator
+  console.log(target);
+}
+
+@Component
+class Car {
+};
