2a4f7cd41fe20e7304ab47bfe83d7663ead5d10d
Daniel Abati <danielabatibabatunde1@gmail.com>
2022-05-18 09:16:50 +0100

    ts-learn

--- ts/index.ts
+++ ts/index.ts
@@ -7,3 +7,7 @@ const ten:number = -10;
 const nothingHere:null = null;
 
 const noDefinition:undefined = undefined;
+
+const joinstring = function(a:string, b:string):string {
+  return 'Dhaniel'
+}
